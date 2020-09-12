13a5c81f8563c47c2c8c498aabb3484e809cfaaf
William Fullerton <wrk.fullerton@gmail.com>
2020-09-12 10:10:35 -0500

    added the routes and paths for the various components

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ToastContainer } from "react-toastify";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Search from "./pages/search";
+import Saved from "./pages/save";
+import NotFound from "./pages/notFound";
+import Footer from "./components/Footer/Footer"
+import NavBar from "./components/NavBar/NavBar";
 import './App.css';
-import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Saved from "./components/Saved";
-import Search from "./components/Search";
-import NotFound from "./components/NotFound";
-import Footer from "./components/Footer";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
-  return (
-    <div className="app">
-      <Router>
-   
-        <NavBar />  
-
-        <Switch>
-
-            <Route>
-            <NotFound/>
-            </Route>
-
-            <Route>
-              <Saved/>
-            </Route>            
-
-
-            <Route>
-              <Search/>
-            </Route>
-
-            <Route>
-              <Search/>
-            </Route>
-
-        </Switch>
-
-      <Footer/>
-
-    </Router>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+<React.Fragment>
+<ToastContainer />
+<section className="hero-is-fullheight">
+  <div className="head-head">
+    <NavBar />
+  </div>
+  <div className="head-body">
+    <Switch>
+      <Route path="/search" components={Search} />
+      <Route path="/saved" components={Saved} />
+      <Route path="/not-found" components={NotFound} />
+      <Redirect from="/" exact to="/search"/>
+      <Redirect to="/not-found"/>
+    </Switch>
+  </div>
+  <div className="head-foot">
+      <Footer />
+      </div>
+    </section>
+  </React.Fragment>    
+    );
+  }
 }
 
 export default App;
